1c737e36e1210594500478322dbdcb7809bd2340
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-09-19 09:51:48 +0200

    Updated Structure

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,49 +1,48 @@
 import Booking from "../models/Booking.js";
 
-// create new booking
 export const createBooking = async (req, res) => {
-  const newBooking = new Booking(req.body);
-  try {
-    const savedBooking = await newBooking.save();
+    const newBooking = new Booking(req.body);
 
-    res.status(200).json({
-      success: true,
-      message: "Your tour is booked",
-      data: savedBooking,
-    });
-  } catch (err) {
-    res.status(500).json({ success: true, message: "internal server error" });
-  }
+    try {
+        const savedBooking = await newBooking.save();
+        res.status(200).json({
+            success: true,
+            message: "Your tour is booked",
+            data: savedBooking,
+        });
+
+    } catch (err) {
+        res.status(500).json({success: true, message: "internal server error"});
+    }
 };
 
-// get single booking
 export const getBooking = async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    const book = await Booking.findById(id);
+    try {
+        const book = await Booking.findById(id);
+        res.status(200).json({
+            success: true,
+            message: "successful",
+            data: book,
+        });
 
-    res.status(200).json({
-      success: true,
-      message: "successful",
-      data: book,
-    });
-  } catch (err) {
-    res.status(404).json({ success: true, message: "not found" });
-  }
+    } catch (err) {
+        res.status(404).json({success: true, message: "not found"});
+    }
 };
 
-// get all booking
 export const getAllBooking = async (req, res) => {
-  try {
-    const books = await Booking.find();
+    try {
+        const books = await Booking.find();
 
-    res.status(200).json({
-      success: true,
-      message: "successful",
-      data: books,
-    });
-  } catch (err) {
-    res.status(500).json({ success: true, message: "internal server error" });
-  }
+        res.status(200).json({
+            success: true,
+            message: "successful",
+            data: books,
+        });
+
+    } catch (err) {
+        res.status(500).json({success: true, message: "internal server error"});
+    }
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,102 +2,102 @@ import User from "../models/User.js";
 
 // create new User
 export const createUser = async (req, res) => {
-  const newUser = new User(req.body);
+    const newUser = new User(req.body);
 
-  try {
-    const savedUser = await newUser.save();
+    try {
+        const savedUser = await newUser.save();
 
-    res.status(200).json({
-      success: true,
-      message: "Successfully created",
-      data: savedUser,
-    });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ success: false, message: "Failed to create. Try again" });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Successfully created",
+            data: savedUser,
+        });
+    } catch (err) {
+        res
+            .status(500)
+            .json({success: false, message: "Failed to create. Try again"});
+    }
 };
 
 // update User
 export const updateUser = async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            {
+                $set: req.body,
+            },
+            {new: true}
+        );
 
-    res.status(200).json({
-      success: true,
-      message: "Successfully updated",
-      data: updatedUser,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "failed to update",
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Successfully updated",
+            data: updatedUser,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "failed to update",
+        });
+    }
 };
 
 // delete User
 export const deleteUser = async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    await User.findByIdAndDelete(id);
+    try {
+        await User.findByIdAndDelete(id);
 
-    res.status(200).json({
-      success: true,
-      message: "Successfully deleted",
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "failed to delete",
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Successfully deleted",
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "failed to delete",
+        });
+    }
 };
 
 // getSingle User
 export const getSingleUser = async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    const user = await User.findById(id);
+    try {
+        const user = await User.findById(id);
 
-    res.status(200).json({
-      success: true,
-      message: "Successful",
-      data: user,
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      message: "not found",
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Successful",
+            data: user,
+        });
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "not found",
+        });
+    }
 };
 
 // getAll User
 export const getAllUser = async (req, res) => {
-  try {
-    const users = await User.find({});
+    try {
+        const users = await User.find({});
 
-    res.status(200).json({
-      success: true,
-      message: "Successful",
-      data: users,
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      message: "not found",
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Successful",
+            data: users,
+        });
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "not found",
+        });
+    }
 };
