c9095f47a32ca37aedb0270df31ad26481bf6436
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-09-19 13:23:22 +0200

    Updated Cards

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -4,28 +4,26 @@ import TourCard from "../../shared/TourCard";
 import { Col } from "reactstrap";
 
 import useFetch from "./../../hooks/useFetch";
-import { BASE_URL } from "./../../utils/config";
+import { BASE_URL } from "../../utils/config";
 
 const FeaturedTourList = () => {
-  const {
-    data: featuredTours,
-    loading,
-    error,
-  } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+    const {
+        data: featuredTours,
+        loading,
+        error,
+    } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
 
-  return (
-    <>
-      {loading && <h4>Loading...........</h4>}
-      {error && <h4>{error}</h4>}
-      {!loading &&
-        !error &&
-        featuredTours?.map(tour => (
-          <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
-            <TourCard tour={tour} />
-          </Col>
-        ))}
-    </>
-  );
+    return (
+        <React.Fragment>
+            {loading && <h4>Loading...........</h4>}
+            {error && <h4>{error}</h4>}
+            {!loading && !error && featuredTours?.map(tour => (
+                    <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}>
+                        <TourCard tour={tour}/>
+                    </Col>
+            ))}
+        </React.Fragment>
+    );
 };
 
 export default FeaturedTourList;
--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -5,34 +5,34 @@ import { Container, Row, Col } from "reactstrap";
 import maleTourist from "../assets/images/male-tourist.png";
 
 const Newsletter = () => {
-  return (
-    <section className="newsletter">
-      <Container>
-        <Row>
-          <Col lg="6">
-            <div className="newsletter__content">
-              <h2>Subscribe now to get useful traveling information.</h2>
+    return (
+        <section className="newsletter">
+            <Container>
+                <Row>
+                    <Col lg="6">
+                        <div className="newsletter__content">
+                            <h2>Subscribe now to get useful travelling information.</h2>
 
-              <div className="newsletter__input">
-                <input type="email" placeholder="Enter your email" />
-                <button className="btn newsletter__btn">Subscribe</button>
-              </div>
+                            <div className="newsletter__input">
+                                <input type="email" placeholder="Enter your email"/>
+                                <button className="btn newsletter__btn">Subscribe</button>
+                            </div>
 
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati adipisci sunt in, provident facere ipsam.
-              </p>
-            </div>
-          </Col>
-          <Col lg="6">
-            <div className="newsletter__img">
-              <img src={maleTourist} alt="" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Obcaecati adipisci sunt in, provident facere ipsam.
+                            </p>
+                        </div>
+                    </Col>
+                    <Col lg="6">
+                        <div className="newsletter__img">
+                            <img src={maleTourist} alt=""/>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 };
 
 export default Newsletter;
